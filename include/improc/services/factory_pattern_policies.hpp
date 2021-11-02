07a05f14824dcfad9ab6e4b83d3b4ff921c9bb7c
pirlruc <pirlruc@gmail.com>
2021-11-02 00:32:51 +0000

    Moved exception to header file with exceptions

--- include/improc/services/factory_pattern_policies.hpp
+++ include/improc/services/factory_pattern_policies.hpp
@@ -6,20 +6,10 @@ namespace improc
     template <typename KeyType, class BaseProduct>
     class FactoryPatternError
     {
-        public:
-            class not_found_type : public std::exception
-            {
-                public:
-                    const char* what() const throw()
-                    {
-                        return "Unknown object type passed to factory.";
-                    }
-            };
-        
         protected:
             static std::shared_ptr<BaseProduct> OnUnknownType(const KeyType&)
             {
-                throw improc::FactoryPatternError<KeyType,BaseProduct>::not_found_type();
+                throw improc::not_found_in_factory();
             }
     };
 }
